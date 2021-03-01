1bede931378746ea7579721e302b57d5a9301c50
Marina Dariy <dariy.mvs@gmail.com>
2021-03-01 19:50:51 +0300

    add tablet version

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 'use strict'
 
+// Переменные - объекты страницы
 let iconColliction = [...document.querySelectorAll('.col_box_weAreGreat_description_list_item')];
 let reviewsList = document.querySelector('.col_box_reviews_list');
+let burger = document.querySelector('.header_nav_burger');
+let headerMenu = document.querySelector('.header_nav_list');
+let headerImg = document.querySelector('.header_nav_img');
+
+// События
 
 iconColliction.forEach(i => {
   i.onclick = () => {
@@ -26,3 +32,12 @@ document.querySelector('.arrow_right').addEventListener('click', () => {
 document.querySelector('.arrow_left').addEventListener('click', () => {
   reviewsList.scrollBy(-40, 0)
 });
+
+
+burger.addEventListener('click', () => {
+headerMenu.classList.toggle('menu_active');
+})
+
+headerImg.addEventListener('click', () => {
+  window.location.href = '../index.html'
+})
