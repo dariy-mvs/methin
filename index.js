66307a029a6929730915500f5bd39b53472d081c
Marina Dariy <dariy.mvs@gmail.com>
2021-03-05 11:19:59 +0300

    fix mobile version

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict'
 
 // Переменные - объекты страницы
-let iconColliction = [...document.querySelectorAll('.col_box_weAreGreat_description_list_item')];
+let iconColliction = document.querySelector('.col_box_weAreGreat_description_list');
+let priceCollection = document.querySelector('.col_box_programPrice_description_list');
 let reviewsList = document.querySelector('.col_box_reviews_list');
 let burger = document.querySelector('.header_nav_burger');
 let headerMenu = document.querySelector('.header_nav_list');
@@ -9,21 +10,33 @@ let headerImg = document.querySelector('.header_nav_img');
 
 // События
 
-iconColliction.forEach(i => {
-  i.onclick = () => {
-    let questionItem = i.querySelector('.col_box_weAreGreat_description_list_item_descr');
+function showDescr(item, selector) { // Вот эту функцию хочется использовать и в списке "преимуществ" и в списке цен. С преимуществами всё ок.
+  let questionItem = item.querySelector(selector);
+  console.log(questionItem);
     if (questionItem.classList.contains('descr_active')) {
       questionItem.classList.remove('descr_active')
     } else {
-      let activeItem = document.querySelector('.descr_active');
+      let activeItem = questionItem.parentElement.querySelector('.descr_active');
+      console.log(activeItem);
     if (activeItem) {
       activeItem.classList.remove('descr_active')
     }
     questionItem.classList.add('descr_active')
     }
-    
-  }
+}
+
+iconColliction.addEventListener('click', (event) => {
+  let target = event.target;
+  if (!target.closest('li')) return;
+  showDescr(target.closest('li'), '.col_box_weAreGreat_description_list_item_descr')
 });
+
+priceCollection.addEventListener('click', (event) => {
+  let target = event.target;
+  console.log(target.closest('li'));
+  if (!target.closest('li')) return;
+  showDescr(target.closest('li'), '.programPrice_descr_list')
+})
 
 document.querySelector('.arrow_right').addEventListener('click', () => {
   reviewsList.scrollBy(393, 0)
